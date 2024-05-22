41cc61852cbebef47574ec4a809ced2c39d42380
dimpal001 <dimpaldas518@gmail.com>
2024-05-22 17:48:46 +0530

    update footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,14 @@ const Footer = () => {
   return (
     <div className='bg-gray-950'>
       <p className='text-center p-10'>
-        Crafted with passion, designed with flair, and developed with Love ❤️
+        Crafted with passion, designed with flair by{' '}
+        <a
+          className='hover:text-first underline'
+          href='https://www.dimpaldas.in'
+          target='_blank'
+        >
+          Dimpal Das
+        </a>
       </p>
     </div>
   )
