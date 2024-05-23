e750685630761a3a0d15202c69a80f0a6916ffdd
dimpal001 <dimpaldas518@gmail.com>
2024-05-24 03:40:16 +0530

    add footer part

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,6 +3,7 @@ const Footer = () => {
     <div className='bg-gray-950'>
       <p className='text-center p-10'>
         Crafted with passion, designed with flair by{' '}
+        <br className='md:hidden' />
         <a
           className='hover:text-first underline'
           href='https://www.dimpaldas.in'
